1f6d420f89147250efd7e4e16fd7814573d4ef0b
Leo6648 <jchen6648@gmail.com>
2025-10-13 12:56:51 -0500

    Add basics to movie project

---
+++ movie-review-demo/src/main/java/com/movie/review/app/movie_review_demo/MovieReviewDemoApplication.java
@@ -0,0 +1,13 @@
+package com.movie.review.app.movie_review_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewDemoApplication.class, args);
+	}
+
+}
---
+++ movie-review-demo/src/test/java/com/movie/review/app/movie_review_demo/MovieReviewDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.review.app.movie_review_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
